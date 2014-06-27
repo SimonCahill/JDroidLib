ebc70322948adec7d530dffb0e648cad058d0864
Beatsleigher <Beatsleigher@hotmail.com>
2014-06-27 18:59:00 +0200

    Minor change to OS.java.  - Added getOS() method.

--- JDroidLib/src/JDroidLib/enums/OS.java
+++ JDroidLib/src/JDroidLib/enums/OS.java
@@ -6,10 +6,26 @@
 
 package JDroidLib.enums;
 
+import JDroidLib.exceptions.OSNotSupportedException;
+
 /**
  * The operating systems supported by JDroidLib.
  * @author beatsleigher
  */
 public enum OS {
-    WINDOWS, LINUX, MAC_OS
+    WINDOWS, LINUX, MAC_OS;
+    
+    /**
+     * Attempts to return a type of @see OS via a value.
+     * @param val The OS to return
+     * @return A type of @see OS
+     * @throws OSNotSupportedException If no type of @see OS could be determined. 
+     */
+    public OS getOS(String val) throws OSNotSupportedException {
+        for (OS os : OS.values())
+            if ((os.toString()).contains(val))
+                return os;
+        throw new OSNotSupportedException("OS \"" + val + "\" is not supported by JDroidLib!");
+    }
+    
 }
